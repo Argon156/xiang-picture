650212e59385181803b30fff3d4e4912e5603ada
Xiangzi <147356338+Argon156@users.noreply.github.com>
2025-05-26 14:59:46 +0800

    Delete src directory

--- src/main/java/com/xiangzi/xiangpicture/domain/space/repository/SpaceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.xiangzi.xiangpicture.domain.space.repository;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.xiangzi.xiangpicture.domain.space.entity.Space;
-
-public interface SpaceRepository extends IService<Space> {
-}
